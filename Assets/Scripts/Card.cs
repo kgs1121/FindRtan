ea3233660f9505b41902348ffaa948492c79dd9e
kgs1121 <kkggss1121@gmail.com>
2025-01-20 17:56:57 +0900

    Initial commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int idx = 0;
+
+    public GameObject front;
+    public GameObject back;
+
+    public Animator anim;
+
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    public SpriteRenderer frontImage;
+
+    public static bool canOpen = true;// 카드 열려있을때 못열게 막기
+
+
+    void Start()
+    {
+        canOpen = true;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void Setting(int number)
+    {
+        idx = number;
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}");
+    }
+
+    public void OpenCard()
+    {
+        
+
+        if (canOpen)
+        {
+            if (GameManager.Instance.secondCard != null) return;
+
+            audioSource.PlayOneShot(clip);
+            anim.SetBool("isOpen", true);
+            front.SetActive(true);
+            back.SetActive(false);
+
+            // firstCard가 비었다면
+            if (GameManager.Instance.firstCard == null)
+            {
+                // firstCard에 내 정보를 념겨준다.
+                GameManager.Instance.firstCard = this;
+                
+            }
+            // firstCard가 비어있지 않다면 
+            else
+            {
+                //secondCard에 내 정보를 념겨준다.
+                GameManager.Instance.secondCard = this;
+                
+
+                //Matched 함수를 호출해 준다.
+                GameManager.Instance.Matched();
+                canOpen = false;
+            }
+        }
+    }
+
+    public void DestroyCard()
+    {
+        //Debug.Log($"canOpen check: {canOpen}");
+        Invoke("DestroyCardInvoke", 0.8f);
+        
+       
+    } 
+
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+        canOpen = true;
+    }
+
+    public void CloseCard()
+    {
+        //Debug.Log($"canOpen check: {canOpen}");
+        Invoke("CloseCardInvoke", 0.8f);
+        
+    }
+
+    void CloseCardInvoke()
+    {
+        
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+        canOpen = true;
+
+    }
+
+
+    
+}
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Card firstCard;
+    public Card secondCard;
+
+    public Text timeTxt;
+    public GameObject endTxt;
+
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    public int cardCount = 0;
+    float time = 0.0f;
+
+    private void Awake()
+    {
+       
+        if (Instance == null) Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+
+        if (time >= 30.00)
+        {
+            Time.timeScale = 0f;
+            endTxt.SetActive(true);
+        }
+    }
+
+    public void Matched()
+    {
+        
+        if (firstCard.idx == secondCard.idx)
+        {
+            //ÆÄ±«
+            audioSource.PlayOneShot(clip);
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+            cardCount -= 2;
+            if(cardCount == 0)
+            {
+                Time.timeScale = 0f;
+                endTxt.SetActive(true);
+            }
+        }
+        else
+        {
+            //´Ý¾Æ
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+        
+        firstCard = null;
+        secondCard = null;
+        
+
+    }
+
+
+}
