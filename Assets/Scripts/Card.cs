df5ac2422753a2978bee2c762ed81b561e78bc6f
kgs1121 <kkggss1121@gmail.com>
2025-01-20 19:53:48 +0900

    member1

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -35,7 +35,7 @@ public class Card : MonoBehaviour
     public void Setting(int number)
     {
         idx = number;
-        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}");
+        frontImage.sprite = Resources.Load<Sprite>($"member{idx}");
     }
 
     public void OpenCard()
