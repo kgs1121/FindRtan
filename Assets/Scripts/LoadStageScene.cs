ca7208d2b8a550bebc6a19bdaa47235addc5daaf
hee103 <gmlgml3213@gmail.com>
2025-01-21 16:21:45 +0900

    StageSceneLoad

---
+++ Assets/Scripts/LoadStageScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadStageScene : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    public void LoadStage()
+    {
+        SceneManager.LoadScene("StageScene");
+    }
+}
