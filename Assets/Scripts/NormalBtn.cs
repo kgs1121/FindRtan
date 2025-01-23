5222bb5b13f843c3f4976dcf4724577a449133c3
Kim199610 <kk2247401@naver.com>
2025-01-23 11:35:57 +0900

    final

--- Assets/Scripts/NormalBtn.cs
+++ Assets/Scripts/NormalBtn.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class NormalBtn : MonoBehaviour
+public class NormalBtn : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Image fadeImage; 
-    public float fadeSpeed = 1.0f; 
+    public float fadeSpeed = 1.0f;
+    public GameObject explains;
+
+    int BtnIdx;
+    string nameNum;
+
+    private void Start()
+    {
+        nameNum = this.name[0].ToString();
+        BtnIdx=int.Parse(nameNum);
+    }
     public void Retry(int n)
     {
     
@@ -34,4 +45,16 @@ public class NormalBtn : MonoBehaviour
         PlayerPrefs.SetInt("Diff", n);
         SceneManager.LoadScene("MainScene");
     }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GameObject explain = explains.transform.GetChild(BtnIdx).gameObject;
+        explain.SetActive(true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GameObject explain = explains.transform.GetChild(BtnIdx).gameObject;
+        explain.SetActive(false);
+    }
+
 }
